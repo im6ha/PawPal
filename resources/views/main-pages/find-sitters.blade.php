2544e72a38d4f82b79a30d9b178005fe068fbfeb
fadi552556k <ffado6173@gmail.com>
2026-02-08 00:35:50 +0100

    Add CSRF token meta tag to find-sitters page
    hh

--- resources/views/main-pages/find-sitters.blade.php
+++ resources/views/main-pages/find-sitters.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="{{ asset('media/images/logo-brand.png') }}" />
 
     <link rel="stylesheet" href="{{ asset('css/components/header.css') }}" />
